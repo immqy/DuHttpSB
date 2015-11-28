56e5cea7ccfa591854727bc5de07fc4317be7724
杜锦阳 <309933706@qq.com>
2015-11-28 10:56:07 +0800

    提交代码

---
+++ app/src/main/java/com/mqy/yang/duyang/com/mqy/yang/utils/DuNumsUtils.java
@@ -0,0 +1,25 @@
+package com.mqy.yang.duyang.com.mqy.yang.utils;
+
+import android.util.Log;
+
+/**
+ * Created by pengjianbo on 2015/11/28.
+ */
+public class DuNumsUtils {
+
+    private static final String TAG = "test";
+
+    public static void getNo1(){
+        Log.i(TAG,"我的第一");
+    }
+    public static void getNo2(){
+        Log.i(TAG,"我是第二");
+    }
+    public static void getNo3(){
+        Log.i(TAG,"我是第三");
+    }
+    public static void getNo4(){
+        Log.i(TAG,"我是第四");
+    }
+
+}
